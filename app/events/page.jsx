0087f03ad03039e9d1c67693c29f5e1cd8e278ee
Rahul Puri <rahul.puri567@gmail.com>
2026-05-04 19:20:42 +0530

    fix: proper server/client separation for events page

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,8 +1,6 @@
-"use client";
+import EventsContent from "@/components/EventsContent";
 
 export const dynamic = "force-dynamic";
-
-import EventsContent from "@/components/EventsContent";
 
 export default function EventsPage() {
   return <EventsContent />;
