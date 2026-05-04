b1ba0841b85cae6f30cd25b5107124452a30e7f1
Rahul Puri <rahul.puri567@gmail.com>
2026-05-04 18:52:13 +0530

    fix: force dynamic rendering for events route

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,5 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
+import EventsContent from "@/components/EventsContent";
+
 export default function EventsPage() {
-  return <div>Events working</div>;
+  return <EventsContent />;
 }
